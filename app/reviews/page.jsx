6c319c103f00e0a45ee8d596d0ada5cf3fd48e2a
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2024-05-04 21:52:10 +0300

    Add search box - Use is client check to render a component only on client - Make client API calls in useEffect

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -4,6 +4,7 @@ import { getReviews } from "@/lib/reviews";
 import Image from "next/image";
 import Heading from "@/components/Heading";
 import PaginationBar from "@/components/PaginationBar";
+import SearchBox from "@/components/SearchBox";
 
 // export const revalidate = 30; // seconds
 
@@ -19,7 +20,10 @@ const ReviewsPage = async ({ searchParams }) => {
   return (
     <>
       {/* <Heading>Reviews</Heading> */}
-      <PaginationBar href="/reviews" page={page} pageCount={pageCount} />
+      <div className="flex justify-between pb-3">
+        <PaginationBar href="/reviews" page={page} pageCount={pageCount} />
+        <SearchBox />
+      </div>
       <ul className="flex flex-row flex-wrap gap-3">
         {reviews.map((review, index) => (
           <li
---
+++ components/SearchBox.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Combobox } from "@headlessui/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useIsClient } from "@/lib/hooks";
+import { searchReviews } from "@/lib/reviews";
+
+export default function SearchBox() {
+  const router = useRouter();
+  const isClient = useIsClient();
+  const [query, setQuery] = useState("");
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    if (query.length > 1) {
+      (async () => {
+        const reviews = await searchReviews(query);
+        setReviews(reviews);
+      })();
+    } else {
+      setReviews([]);
+    }
+  }, [query]);
+
+  const handleChange = (review) => {
+    router.push(`/reviews/${review.slug}`);
+  };
+
+  // console.log('[SearchBox] query:', query);
+  if (!isClient) {
+    return null;
+  }
+  return (
+    <div className="relative w-48">
+      <Combobox onChange={handleChange}>
+        <Combobox.Input
+          placeholder="Search…"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          className="border px-2 py-1 rounded w-full"
+        />
+        <Combobox.Options className="absolute bg-white py-1 w-full">
+          {reviews.map((review) => (
+            <Combobox.Option key={review.slug} value={review}>
+              {({ active }) => (
+                <span
+                  className={`block px-2 truncate w-full ${
+                    active ? "bg-orange-100" : ""
+                  }`}
+                >
+                  {review.title}
+                </span>
+              )}
+            </Combobox.Option>
+          ))}
+        </Combobox.Options>
+      </Combobox>
+    </div>
+  );
+}
