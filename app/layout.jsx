bb3d5e0ebfbe00a6d000b5f804aa6ce0b1a686ea
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2024-05-06 13:49:20 +0300

    Fix wrong refrence for the metadata for each game page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import { homePageMetadata } from "@/lib/generateMetadataFactory";
 import NavBar from "../components/NavBar";
 import { exo2, orbitron } from "./fonts";
 import "./globals.css";
@@ -5,26 +6,7 @@ import "./globals.css";
 // import { Metadata } from 'next'
 
 export const metadata /*: Metadata*/ = {
-  title: {
-    template: "%s | Indie Game",
-    default: "Indie Game Reviews", // a default is required when creating a template
-  },
-  openGraph: {
-    title: "Indie Game",
-    description:
-      "Indie Game reviews, stay tuned to know everything about the game",
-    url: "https://iknwoly.com",
-    siteName: "Indie Game",
-    images: [
-      {
-        url: "https://indie-games-reviews.vercel.app/_next/image?url=https%3A%2F%2Fnext-reviews-cms.encoded.io%2Fuploads%2Ffall_guys_9f9b386677.jpg&w=1920&q=75", // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
+  ...homePageMetadata,
 };
 
 const layout = ({ children }) => {
